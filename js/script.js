c37e25c468939d713a72647b436fcd4067294fcd
Константин Башков <kostyabashkov@gmail.com>
2021-11-18 15:40:25 +0200

    fix script upload

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-//     spaceBetween: 24,
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     slidesPerView: 4,
-//     breakpoints: {
-//       320: {
-//         slidesPerView: 1
-//       },
-//       520: {
-//         slidesPerView: 2
-//       },
-//       768: {
-//         slidesPerView: 3
-//       },
-//       1198: {
-//         slidesPerView: 4
-//       }
-//     }
-//   });
-
-
 window.addEventListener('load', function() {
   new Glider(document.querySelector('.glider'), {
     slidesToShow: 1,
